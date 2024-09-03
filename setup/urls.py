5a37735c291739c729f44d26dc29ab27c2d7fa91
Nicole de Assis <nicassissiqueira@gmail.com>
2024-09-03 00:20:05 -0300

    Configurando Setup Django

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path
 from ToDos.views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home),
+    path("admin/", admin.site.urls),
+    path("", home),
 ]
